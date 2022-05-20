5b2bb1908dd61ddec80103db285dd4b95608500f
Koen de Raad <koen_de_raad@hotmail.com>
2022-05-20 13:04:46 +0200

    initial setup

---
+++ test/run_model.py
@@ -0,0 +1,16 @@
+import torch
+
+# Model
+# model = torch.hub.load('C:/Users/koen_/OneDrive/Bureaublad/klieder_folder', 'best.pt')
+model = torch.hub.load('C:/Users/koen_/GitHub/yolov5', 'custom', path='C:/Users/koen_/OneDrive/Bureaublad/klieder_folder/best.pt', source='local')  # local repo
+# Image
+im1 = 'https://uploads-ssl.webflow.com/613214e333d406adbe3c4fa3/624d4151097ff22b841869a3_teamtv-automatic-camera-hockey-1.jpg'
+im2 = 'https://uploads-ssl.webflow.com/613214e333d406adbe3c4fa3/624d4151e08ab2bb95f3711f_teamtv-automatic-camera-hockey-2.jpg'
+im3 = 'https://uploads-ssl.webflow.com/613214e333d406adbe3c4fa3/624d4150b7135549bf532330_teamtv-automatic-camera-hockey-3.jpg'
+im4 = 'https://uploads-ssl.webflow.com/613214e333d406adbe3c4fa3/624d4150bb6b4c44f79d6714_teamtv-automatic-camera-hockey-4.jpg'
+
+# Inference
+results = model([im1, im2, im3, im4])
+results.print()
+results.save()
+print(results.pandas().xyxy[0])
---
+++ test/visualize_results.py
@@ -0,0 +1,2 @@
+from utils.plots import plot_results
+plot_results(file='runs/train/exp17')  # plot results.txt as results.png
