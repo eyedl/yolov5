3441feeab869af2f4f9a2213c7e1ca9b7e3d3655
Koen de Raad <koen_de_raad@hotmail.com>
2022-05-23 08:56:00 +0200

    Update dataset_trim_no_balls.py

--- test/dataset_trim_no_balls.py
+++ test/dataset_trim_no_balls.py
@@ -1,2 +1,55 @@
 import os
+import random
+import argparse
+# data_loc = r'C:\Users\koen_\OneDrive\Documents\.Koen de Raad\- Whitebox Data Science\Projecten\TeamTV\Trained Models\Hockey\detectors\dataset_hockey_ball_detection_txt'
+
+if __name__ == '__main__':
+    # def_data = r'C:\Users\koen_\OneDrive\Documents\.Koen de Raad\- Whitebox Data Science\Projecten\TeamTV\Trained Models\Hockey\detectors\dataset_hockey_ball_detection_txt'
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--data', type=str, default='/home/teamtv/eyedle-training/darknet/data/hockeyball_32tr_6te', help='path to dataset folder')
+    parser.add_argument('-f', '--folder', type=str, default='task', help='folder name to filter between train and tests')
+    parser.add_argument('-p', '--pct_empty', type=float, default=0.05, help='percentage of empty files that is needed')
+    args = parser.parse_args()
+
+    data_loc = args.data
+    empty_label_pct = args.pct_empty
+    folder_filter = args.folder
+
+
+    # below we walk through all txt files and check whether they have any label or not
+    empty_labels = []
+    filled_labels = []
+    for root, dirs, files in os.walk(data_loc):
+        if root.split('\\')[-1].startswith(folder_filter):  # specify the name of the training folders
+            if files:
+                for file in files:
+                    if file.endswith('.txt'):
+                        file_loc = os.path.join(root,file)
+                        file_size = os.stat(file_loc).st_size
+                        if file_size > 0:
+                            filled_labels.append(file_loc.replace('.txt', '.png'))
+                        else:
+                            empty_labels.append(file_loc.replace('.txt', '.png'))
+
+    # making sure everything is randomly shuffled
+    random.shuffle(filled_labels)
+    random.shuffle(empty_labels)
+
+    # calculating exact number of empty labels needed given a percentage
+    len_filled_labels = len(filled_labels)
+    len_empty_labels = round((len_filled_labels * empty_label_pct) / (1 - empty_label_pct))
+
+    # with open('/home/teamtv/eyedle-training/darknet/data/hockeyball_32tr_6te/yolov5_hockeyball_32tr_6te_train_filled_unbalanced.txt', 'w+') as f:
+    with open(r'C:\Users\koen_\OneDrive\Bureaublad\klieder_folder\yolov5_hockeyball_32tr_6te_train_filled_unbalanced.txt', 'w+') as f:
+
+        # adding all the filled labels
+        for ix, i in enumerate(filled_labels):
+            f.write(i+'\n')
+
+        # adding the required pct of empty labels
+        for ix, i in enumerate(empty_labels):
+            f.write('EMPTY_'+i+'\n')
+            if ix == len_empty_labels:
+                break
+
 
