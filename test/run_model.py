5175f56fb34cb9d6fe80af445951bcb0fcfabfca
Koen de Raad <koen_de_raad@hotmail.com>
2022-05-25 10:21:51 +0200

    updated map metrrics

--- test/run_model.py
+++ test/run_model.py
@@ -2,7 +2,7 @@ import torch
 
 # Model
 # model = torch.hub.load('C:/Users/koen_/OneDrive/Bureaublad/klieder_folder', 'best.pt')
-model = torch.hub.load('C:/Users/koen_/GitHub/yolov5', 'custom', path='C:/Users/koen_/OneDrive/Bureaublad/klieder_folder/best.pt', source='local')  # local repo
+model = torch.hub.load('C:/Users/koen_/GitHub/yolov5', 'custom', path='C:/Users/koen_/OneDrive/Documents/.Koen de Raad/- Whitebox Data Science/Projecten/TeamTV/Trained Models/Hockey/detectors/exp_24-05/best.pt', source='local')  # local repo
 # Image
 im1 = 'https://uploads-ssl.webflow.com/613214e333d406adbe3c4fa3/624d4151097ff22b841869a3_teamtv-automatic-camera-hockey-1.jpg'
 im2 = 'https://uploads-ssl.webflow.com/613214e333d406adbe3c4fa3/624d4151e08ab2bb95f3711f_teamtv-automatic-camera-hockey-2.jpg'
@@ -12,5 +12,5 @@ im4 = 'https://uploads-ssl.webflow.com/613214e333d406adbe3c4fa3/624d4150bb6b4c44
 # Inference
 results = model([im1, im2, im3, im4])
 results.print()
+print(results.pandas().xyxy)
 results.save()
-print(results.pandas().xyxy[0])
